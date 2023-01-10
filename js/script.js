97ba168db40e702c9badbb0c2d3eae88d422d6a4
Moamal-2000 <moamalalaapro1@gmail.com>
2023-01-10 22:52:03 +0300

    new features added to javascript code

--- js/script.js
+++ js/script.js
@@ -1,30 +1,44 @@
 "use strict";
 //* Variables [Start]
-let membersSection = document.querySelector(".layout .members");
-let groupsSection = document.querySelector(".layout .groups");
-let activeMembersSection = document.querySelector(
-  ".layout .recent-active-members"
-);
-let scheduleSection = document.querySelector(".layout .schedule");
-let sliderImgsSection = document.querySelector(".layout .slider-imgs");
-let latestActivitySection = document.querySelector(".layout .latest-activity");
-let postsSection = document.querySelector(".layout .posts");
-let featuresSection = document.querySelector(".layout .features");
-let signInSection = document.querySelector(".layout .sign-in");
-let statisticsSection = document.querySelector(".layout .statistics");
-let whoIsOnlineSection = document.querySelector(".layout .who-is-online");
-let repliesSection = document.querySelector(".layout .replies");
-let topicsSection = document.querySelector(".layout .topics");
-let overlay = document.querySelector(".overlay");
+const membersSection = document.querySelector(".layout .members"),
+  groupsSection = document.querySelector(".layout .groups"),
+  activeMembersSection = document.querySelector(
+    ".layout .recent-active-members"
+  ),
+  scheduleSection = document.querySelector(".layout .schedule"),
+  sliderImgsSection = document.querySelector(".layout .slider-imgs"),
+  latestActivitySection = document.querySelector(".layout .latest-activity"),
+  postsSection = document.querySelector(".layout .posts"),
+  featuresSection = document.querySelector(".layout .features"),
+  signInSection = document.querySelector(".layout .sign-in"),
+  statisticsSection = document.querySelector(".layout .statistics"),
+  whoIsOnlineSection = document.querySelector(".layout .who-is-online"),
+  repliesSection = document.querySelector(".layout .replies"),
+  topicsSection = document.querySelector(".layout .topics"),
+  overlay = document.querySelector(".overlay"),
+  leftSideSections = document.querySelectorAll(".left-side section"),
+  middleSideSections = document.querySelectorAll(".middle-side section"),
+  rightSideSections = document.querySelectorAll(".right-side section"),
+  signInForm = document.querySelector(".right-side .sign-in");
+
+const allSections = [
+  ...leftSideSections,
+  ...middleSideSections,
+  ...rightSideSections,
+  signInForm,
+];
+
+// Unneeded element of search bar
+allSections.shift();
+
 //* Variables [End]
 
 //* Functions [Start]
-function error(ele, color, timer = 400, originColor = "black") {
+function error(ele, color, timer = 400, originalColor = "black") {
   ele.style.transition = "0.3s";
   ele.style.color = color;
   setTimeout(() => {
-    ele.style.color = originColor;
-    ele.style.color = originColor;
+    ele.style.color = originalColor;
   }, timer);
 }
 
@@ -57,10 +71,10 @@ friendsButton.forEach((btn) => {
     btn.style.transitionDelay = "0.3s";
   });
 });
+
 //! Friends Buttons [End]
 
 //? Popup sign up form [Start]
-
 let signUpButton = document.querySelector(".sign-in .create-account");
 let signUpForm = document.querySelector(".sign-up");
 signUpButton.addEventListener("click", () => {
@@ -72,184 +86,53 @@ overlay.addEventListener("click", () => {
   overlay.style.display = "none";
   signUpForm.style.display = "none";
 });
-
 //? Popup sign up form [End]
 
 //! Search Bar [Start]
-let searchInp = document.querySelector(".search-bar input");
-let buttonSearch = document.querySelector(".search-bar button");
+const searchInp = document.querySelector(".search-bar input"),
+  buttonSearch = document.querySelector(".search-bar button");
+let focusedOnSearchBar = false;
+
+window.addEventListener("click", (e) => {
+  e.target.className === "search-bar"
+    ? (focusedOnSearchBar = true)
+    : (focusedOnSearchBar = false);
+
+  if (!focusedOnSearchBar) searchMenu.style.display = "none";
+});
+
+window.addEventListener("keypress", (e) => {
+  e.code === "Enter" && focusedOnSearchBar ? buttonSearch.click() : "";
+});
 
 buttonSearch.addEventListener("click", () => {
-  switch (searchInp.value.toLowerCase()) {
-    case "members": {
+  let correctNameSection = false;
+
+  sectionsTitle.forEach((section) => {
+    if (section.textContent === searchInp.value) {
+      correctNameSection = true;
       window.scrollTo(
         0,
         scrollY +
-          membersSection.offsetHeight +
-          membersSection.offsetTop -
-          innerHeight
+          section.parentElement.offsetHeight +
+          section.parentElement.offsetTop -
+          window.innerHeight
       );
-      sectionEffect(membersSection);
-      searchInp.value = "";
-      break;
+      sectionEffect(section.parentElement);
+      // searchInp.value = "";
     }
+  });
 
-    case "groups": {
-      window.scrollTo(
-        0,
-        scrollY +
-          groupsSection.offsetHeight +
-          groupsSection.offsetTop -
-          innerHeight
-      );
-      sectionEffect(groupsSection);
-      searchInp.value = "";
-      break;
-    }
-
-    case "recently active members": {
-      window.scrollTo(
-        0,
-        scrollY +
-          activeMembersSection.offsetHeight +
-          activeMembersSection.offsetTop -
-          innerHeight
-      );
-      sectionEffect(activeMembersSection);
-      searchInp.value = "";
-      break;
-    }
-
-    case "the schedule": {
-      window.scrollTo(
-        0,
-        scrollY +
-          scheduleSection.offsetHeight +
-          scheduleSection.offsetTop -
-          innerHeight
-      );
-      sectionEffect(scheduleSection);
-      searchInp.value = "";
-      break;
-    }
-
-    case "latest activity": {
-      window.scrollTo(
-        0,
-        scrollY +
-          latestActivitySection.offsetHeight +
-          latestActivitySection.offsetTop -
-          innerHeight
-      );
-      sectionEffect(latestActivitySection);
-      searchInp.value = "";
-      break;
-    }
-
-    case "latest posts": {
-      window.scrollTo(
-        0,
-        scrollY +
-          postsSection.offsetHeight +
-          postsSection.offsetTop -
-          innerHeight
-      );
-      sectionEffect(postsSection);
-      searchInp.value = "";
-      break;
-    }
-
-    case "the features": {
-      window.scrollTo(
-        0,
-        scrollY +
-          featuresSection.offsetHeight +
-          featuresSection.offsetTop -
-          innerHeight
-      );
-      sectionEffect(featuresSection);
-      searchInp.value = "";
-      break;
-    }
-
-    case "sign in": {
-      window.scrollTo(
-        0,
-        scrollY +
-          signInSection.offsetHeight +
-          signInSection.offsetTop -
-          innerHeight
-      );
-      sectionEffect(signInSection);
-      searchInp.value = "";
-      break;
-    }
-
-    case "statistics": {
-      window.scrollTo(
-        0,
-        scrollY +
-          statisticsSection.offsetHeight +
-          statisticsSection.offsetTop -
-          innerHeight
-      );
-      sectionEffect(statisticsSection);
-      searchInp.value = "";
-      break;
-    }
-
-    case "who's online": {
-      window.scrollTo(
-        0,
-        scrollY +
-          whoIsOnlineSection.offsetHeight +
-          whoIsOnlineSection.offsetTop -
-          innerHeight
-      );
-      sectionEffect(whoIsOnlineSection);
-      searchInp.value = "";
-      break;
-    }
-
-    case "recent replies": {
-      window.scrollTo(
-        0,
-        scrollY +
-          repliesSection.offsetHeight +
-          repliesSection.offsetTop -
-          innerHeight
-      );
-      sectionEffect(repliesSection);
-      searchInp.value = "";
-      break;
-    }
-
-    case "recent topics": {
-      window.scrollTo(
-        0,
-        scrollY +
-          topicsSection.offsetHeight +
-          topicsSection.offsetTop -
-          innerHeight
-      );
-      sectionEffect(topicsSection);
-      searchInp.value = "";
-      break;
-    }
-
-    default: {
-      error(searchInp, "red", 500);
-    }
-  }
+  if (!correctNameSection) error(searchInp, "red");
 });
 
 //* Search Menu [Start]
-let sections = document.querySelectorAll(".layout div section h2");
-let searchMenu = document.querySelector(
+const sectionsTitle = document.querySelectorAll(".layout .primary-title");
+const searchMenu = document.querySelector(
   ".layout .left-side .search-bar .search-menu"
 );
 
-sections.forEach((section) => {
+sectionsTitle.forEach((section) => {
   let div = document.createElement("div");
   div.classList.add("search-menu__item");
   let text = document.createTextNode(section.textContent);
@@ -259,30 +142,31 @@ sections.forEach((section) => {
 
 searchInp.addEventListener("input", () => {
   searchMenu.style.display = "flex";
-  if (searchInp.value === "") {
-    searchMenu.style.display = "none";
-  }
+  if (searchInp.value === "") searchMenu.style.display = "none";
 });
 
 searchInp.addEventListener("focus", () => {
   searchMenu.style.display = "flex";
 });
 
+// Clicking on name the section will auto complete the word
+// And do search automatically
 for (let i = 0; i < searchMenu.children.length; i++) {
   searchMenu.children[i].addEventListener("click", () => {
     searchInp.value = searchMenu.children[i].textContent;
+    buttonSearch.click();
   });
 }
 //* Search Menu [End]
 //! Search Bar [End]
 
 //? Validate Password [Start]
-let signUpContainer = document.querySelector(".sign-up");
-let lockIcon = document.querySelector(".sign-up form div .lock");
-let unlockIcon = document.querySelector(".sign-up form div .unlock");
-let passInp = document.querySelector(".sign-up form div .password");
-let confirmPassInp = document.querySelector(".sign-up form div .confirm-pass");
-let signUpBtn = document.querySelector(".sign-up form div .submit");
+const signUpContainer = document.querySelector(".sign-up"),
+  lockIcon = document.querySelector(".sign-up form div .lock"),
+  unlockIcon = document.querySelector(".sign-up form div .unlock"),
+  passInp = document.querySelector(".sign-up form div .password"),
+  confirmPassInp = document.querySelector(".sign-up form div .confirm-pass"),
+  signUpBtn = document.querySelector(".sign-up form div .submit");
 
 lockIcon.addEventListener("click", () => {
   lockIcon.style.display = "none";
@@ -325,9 +209,10 @@ let monthsOfTheYear = [
   "Nov",
   "Dec",
 ];
-year.textContent = `${
-  monthsOfTheYear[new Date().getMonth()]
-} ${new Date().getFullYear()}`;
+year.textContent = `${monthsOfTheYear[new Date().getMonth()]} 
+${new Date().getFullYear()}`;
+
 prevMonth.textContent = monthsOfTheYear[new Date().getMonth() - 1];
+
 days[new Date().getDate() - 1].classList.add("active");
 //! schedule Date [End]
